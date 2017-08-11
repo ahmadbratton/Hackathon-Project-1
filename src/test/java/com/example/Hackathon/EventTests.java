045b0e08e74130ee3ec0b8097d23240d24e49c89
Davis Uhlig <duhlig@daviss-mbp.lan>
2017-08-11 13:55:42 -0400

    added tests for adding event, getting list of events, and get an event by id

--- src/test/java/com/example/Hackathon/EventTests.java
+++ src/test/java/com/example/Hackathon/EventTests.java
@@ -5,7 +5,9 @@ import com.example.Hackathon.model.Status;
 import com.example.Hackathon.model.User;
 import com.example.Hackathon.repository.EventRepo;
 import com.example.Hackathon.repository.UserRepo;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,7 +58,32 @@ public class EventTests {
         }
         Event event1 = findInList(allEvents, "event1", "this is the description", "2017-12-09");
         Assert.assertNotNull(event1);
+
+        Event event2 = new Event();
+        event2.setName("event1");
+        event2.setDate("2017-12-09");
+        event2.setDescription("this is the description");
+        event2.setStatus(Status.NEW);
+        eventRepo.save(event2);
+
+        getAllEvents = eventRepo.findAll();
+        allEvents = new ArrayList<>();
+        for(Event currentEvent : getAllEvents) {
+            allEvents.add(currentEvent);
+        }
+        int numOfEvents = allEvents.size();
+        Assert.assertEquals(numOfEvents, 2);
+
+        Event event3 = eventRepo.findOne(event2.getEventId());
+        Assert.assertNotNull(event3);
+        Assert.assertEquals("event1", event3.getName());
+        Assert.assertEquals("2017-12-09", event3.getDate());
+        Assert.assertEquals("this is the description", event3.getDescription());
+        Assert.assertEquals(Status.NEW, event3.getStatus());
     }
+
+
+
     public static Event findInList(List<Event> events, String name, String description, String date) {
         boolean found = false;
         for (Event event : events) {
